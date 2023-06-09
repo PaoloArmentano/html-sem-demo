18884a40ffc7a882587903f6ed72c90d5568a7a1
PaoloArmentano <armentano.paolo@gmail.com>
2023-06-09 11:51:11 +0100

    added some JS

--- script.js
+++ script.js
@@ -2,3 +2,25 @@ function changeColor() {
     let heading = document.getElementById('heading');
     heading.classList.toggle('pink');
   }
+
+  function yourName(){
+    let user = prompt("What's your name?");
+    return document.write(user);
+  }
+
+function timeOfDay(){
+  let time = prompt("What hour is it? (0 - 23)");
+  let location = prompt("What country are you in?");
+  let message ="";
+
+if(time <= 11 || location === 'england'){
+  message = "Good Morning";
+} else if(time <= 18 || location === "england"){
+  message = "Good Evening";
+} else if(time <= 18 || location === "england"){
+  message = "Good Afternoon";
+} else{
+  message = "Good Afternoon";
+}
+return document.write(message);
+}
